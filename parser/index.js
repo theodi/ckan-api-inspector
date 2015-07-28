8d518c6ea7a844b5ec44ab1e178a4af9e88de3d6
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-07-28 14:11:10 +0100

    Use package_search
    Now using `package_search` instead of `package_list` as data.gov did
    not support the latter.
    
    Also `package_search` actually speeds up parsing as it returns 1000
    packages per request, so it is a better idea anyway.

--- parser/index.js
+++ parser/index.js
@@ -1,6 +1,5 @@
 import "colors";
 import _ from "lodash";
-import Q from "q";
 import Consumer from "./consumer";
 import getJSON from "./getJSON";
 
@@ -8,44 +7,44 @@ export default class Parser {
 
   constructor({ url, limit }) {
     this.url = url;
+    this.processed = 0;
     this.limit = limit;
     this.consumer = new Consumer();
+    this.getUntilComplete = this.getUntilComplete.bind(this);
   }
 
-  getIds() {
-    return getJSON(`${this.url}/api/3/action/package_list`).then(json => {
-      let results = shuffle(json.result);
-      return this.limit > 0 ? results.slice(0, this.limit) : results;
+  getResult(start, count) {
+    let URL = `${this.url}/api/3/action/package_search?start=${start}&rows=${count}`;
+    console.log(`requesting ${URL}`);
+    return getJSON(URL).then(json => {
+      if (!json.success) {
+        throw new Error(`Search was unsuccessful for ${URL}`);
+      }
+      return json.result;
     });
+  }
+
+  getCount() {
+    return this.getResult(0,0).then(json => json.count);
   }
 
   parseAll() {
-    return this.getIds().then(ids => {
-      console.log(`Processing ${ids.length} datasets from ${this.url}`.bold);
-      let requests = _.map(ids, id => getJSON(`${this.url}/api/2/rest/package/${id}`).then(result => {
-        console.log(`received ${id}`.green);
-        this.consumer.consume(result);
-        return result;
-      }, error => {
-        console.log(`failed ${id}`.red, error);
-        return error;
-      }));
-      return Q.allSettled(requests).then(() => this.consumer.toJSON());
+    return this.getCount().then(count => {
+      if (this.limit > 0) this.limit = Math.min(this.limit, count);
+      else this.limit = count;
+      return this.getUntilComplete();
     });
   }
-}
 
-// From: http://bost.ocks.org/mike/shuffle/
-function shuffle(array) {
-  var m = array.length, t, i;
-  // While there remain elements to shuffle…
-  while (m) {
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
+  getUntilComplete(json) {
+    if (json) {
+      this.processed += json.results.length;
+      _.each(json.results, result => this.consumer.consume(result));
+    }
+    if (this.processed < this.limit) {
+      return this.getResult(this.processed, this.limit - this.processed).then(this.getUntilComplete);
+    } else {
+      return this.consumer.toJSON();
+    }
   }
-  return array;
 }
